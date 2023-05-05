a4c0affff6814632423f242b1639cfd04a2641f4
kbaba1001 <kbaba1001@users.noreply.github.com>
2023-05-05 11:45:59 +0900

    feat: reitit routing

--- src/helix_init/app.cljs
+++ src/helix_init/app.cljs
@@ -2,7 +2,10 @@
   (:require [helix.core :refer [defnc $]]
             [helix.hooks :as hooks]
             [helix.dom :as d]
-            ["react-dom/client" :as rdom]))
+            ["react-dom/client" :as rdom]
+            [reitit.frontend :as rf]
+            [reitit.frontend.easy :as rfe]
+            [reitit.coercion.spec :as rss]))
 
 ;; define components using the `defnc` macro
 (defnc greeting
@@ -11,7 +14,7 @@
   ;; use helix.dom to create DOM elements
   (d/div "Hello, " (d/strong name) "!"))
 
-(defnc app []
+(defnc home-page []
   (let [[state set-state] (hooks/use-state {:name "Helix User"})]
     (d/div
      (d/h1 {:class-name "text-4xl"} "Welcome!")
@@ -20,7 +23,31 @@
      (d/input {:value (:name state)
                :on-change #(set-state assoc :name (.. % -target -value))}))))
 
+(defnc about-page []
+  (d/div
+   (d/h1 {:class-name "text-4xl"} "About")
+   (d/p "This is a Helix app.")))
+
 (defonce root (rdom/createRoot (js/document.getElementById "app")))
 
+(defonce match (atom nil))
+
+(defnc current-page []
+  (d/div
+   (d/h1 {:class-name "text-4xl"} "Header")
+   (when @match
+     (let [view (get-in @match [:data :view])]
+       ($ view @match)))))
+
+(def routes
+  [["/" {:name "home"
+         :view home-page}]
+   ["/about/" {:name "about"
+               :view about-page}]])
+
 (defn ^:export init []
-  (.render root ($ app)))
+  (rfe/start!
+   (rf/router routes)
+   (fn [m] (reset! match m))
+   {:use-fragment false})
+  (.render root ($ current-page)))
