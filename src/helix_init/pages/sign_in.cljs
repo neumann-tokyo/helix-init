a81a62762b87f01c78f5e2e39767c88e4bcf249b
kbaba1001 <kbaba1001@users.noreply.github.com>
2023-05-10 00:50:54 +0900

    feat: react-hook-form

--- src/helix_init/pages/sign_in.cljs
+++ src/helix_init/pages/sign_in.cljs
@@ -6,13 +6,16 @@
 (defn on-submit [data]
   (js/console.log data))
 
+;; https://github.com/dvingo/malli-react-hook-form
 ;; const { register, handleSubmit, watch, formState: { errors } } = useForm();
 (defnc sign-in-page []
   (let [form (useForm)
         register (.-register form)
         handleSubmit (.-handleSubmit form)
-        watch (.-watch form)
-        errors (-> form .-formState .-errors)]
+        errors (-> form .-formState .-errors)
+        register-email (register "email" #js {:required true})
+        register-password (register "password" #js {:required true})]
+    (print errors)
     (d/div
      (d/h1 {:class-name "text-4xl"} "Sign In")
      (d/form
@@ -23,19 +26,29 @@
        (d/label {:for "email"} "Email")
        (d/input
         {:type "email"
-         :name "email"
          :id "email"
          :class-name "border border-gray-300 rounded-md p-2"
-         :placeholder "Email"}))
+         :placeholder "Email"
+         :name (.-name register-email)
+         :on-blur (.-onBlur register-email)
+         :on-change (.-onChange register-email)
+         :ref (.-ref register-email)})
+       (when (.-email errors)
+         (d/span {:class-name "text-red-500"} "This field is required")))
       (d/div
        {:class-name "flex flex-col gap-2"}
        (d/label {:for "password"} "Password")
        (d/input
         {:type "password"
-         :name "password"
          :id "password"
          :class-name "border border-gray-300 rounded-md p-2"
-         :placeholder "Password"}))
+         :placeholder "Password"
+         :name (.-name register-password)
+         :on-blur (.-onBlur register-password)
+         :on-change (.-onChange register-password)
+         :ref (.-ref register-password)})
+       (when (.-password errors)
+         (d/span {:class-name "text-red-500"} "This field is required")))
       (d/button
        {:type "submit"
         :class-name "bg-blue-500 text-white rounded-md p-2"}
