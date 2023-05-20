0b4825871c012f494edb041f3972bdd36a038857
kbaba1001 <kbaba1001@users.noreply.github.com>
2023-05-20 10:29:56 +0900

    feat: axios

--- src/helix_init/components/random_cat.cljs
+++ src/helix_init/components/random_cat.cljs
@@ -1,25 +1,25 @@
 (ns helix-init.components.random-cat
-  (:require [helix.core :refer [defnc $]]
+  (:require [helix.core :refer [defnc]]
             [helix.dom :as d]
-            [cljs.core.async :refer [go <!]]
-            [cljs-http.client :as http]
+            ["axios$default" :as axios]
             ["jotai" :as jotai]
             ["jotai/utils" :as jotai-utils]))
 
-;; TODO cljs-http 用の loadable を自作する
 (defonce random-cat-api-atom
-  (jotai/atom
-   (go (fn [_get] (<! (http/get "https://cataas.com/cat"
-                                {:with-credentials? false
-                                 :query-params {"json" true}}))))))
+  (jotai/atom (axios/get
+               "https://cataas.com/cat"
+               #js {:withCredentials false
+                    :params #js {:json true}})))
 (defonce random-cat-api-loadable-atom
   (jotai-utils/loadable random-cat-api-atom))
 
 (defnc random-cat []
-  (let [[random-cat-api] (jotai/useAtom random-cat-api-loadable-atom)]
-    (print (.-data random-cat-api))
-    (d/div
-     #_(case (.-state random-cat-api)
-         "loading" ($ d/div "Loading...")
-         "hasError" ($ d/div (.-error random-cat-api))
-         (d/div (.-data random-cat-api))))))
+  (let [[random-cat-api] (jotai/useAtom random-cat-api-loadable-atom)
+        image-url (some-> (.-data random-cat-api)
+                          (js->clj :keywordize-keys true)
+                          (get-in [:data :url])
+                          ((fn [path]
+                             (str "https://cataas.com" path))))]
+    (if image-url
+      (d/img {:src image-url :alt "cat"})
+      (d/div "Loading..."))))
